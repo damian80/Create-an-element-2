1f13ccc48242896056c761fa1bde2e2f62db4e4d
Damian Ciasnocha <daneek80@gmail.com>
2021-07-15 20:33:07 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+const btn = document.querySelector('button');
+let number = 1;
+
+const addElement=()=>{
+
+
+
+const li = document.createElement('li');
+li.textContent = number;
+if(number % 3 == 0){
+    li.classList.add('circle')
+}
+document.body.appendChild(li);
+number +=2;
+}
+
+
+
+
+btn.addEventListener('click', addElement)
